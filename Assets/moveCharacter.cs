198617953fa5e63b82daf010300f2da94e91af68
bnezuld <bnezuld@yahoo.com>
2023-02-15 22:06:39 -0600

    character movement
    jumping and moving left/right and down too
    
    next steps:
    stop from running off screen
    
    add timer
    
    add win condition
    
    attach script to camera to build screen on start with timer, floor/walls around the screen, and what game type it is?

--- Assets/moveCharacter.cs
+++ Assets/moveCharacter.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class moveCharacter : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float thrust = 200.0f;
+    private bool isGrounded = false;
+    
+
+    void Awake()
+    {
+        // rb = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        // rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // Debug.Log(collision.gameObject.tag);
+        if(collision.gameObject.tag == "ground"){
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground"){
+            isGrounded = false;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("d"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKey("a"))
+        {
+            transform.position += -1 * transform.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKey("w"))
+        {
+            if(isGrounded)
+            {
+                GetComponent<Rigidbody2D>().AddForce(transform.up * thrust);
+            }
+        }        
+        if(Input.GetKey("s"))
+        {
+            transform.position += -1 * transform.up * speed * Time.deltaTime;
+        }
     }
 }
